cb8409e3358f0f6e4d970b72cdd9feffd68b5d82
Cheri Jones <88801930+CheriJones@users.noreply.github.com>
2024-05-06 12:10:21 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<center>
+<form action="loginpost.php" method="post" name="login" target="_self">
+<table border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+    	<input name="user" type="text" />
+    </td>
+  </tr>
+  <tr>
+    <td>
+    	<input name="pass" type="text" />
+    </td>
+  </tr>
+  <tr>
+    <td>
+    	<input name="b1" type="submit" />    </td>
+  </tr>
+</table>
+</form>
+</center>
+
+
+
+
+
+
+</body>
+</html>
