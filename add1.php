c35945d6af5bb99eae830116972936394654dee9
Cheri Jones <88801930+CheriJones@users.noreply.github.com>
2024-07-06 18:21:08 -0500

    Update add1.php

--- add1.php
+++ add1.php
@@ -1,39 +1,173 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-<body>
-<!--- get username and password from database and compare to passed along --->
-<?php
-	$servername = "mysql24.ezhostingserver.com";
-	$username = "cheri";
-	$password = "M@keC0ntact";
-	$dbname = "btgwwtest";
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-  		die("Connection failed: " . $conn->connect_error);
-	}
-
-	$getpass = $_GET["passwords"];
-		
-	$sql = "SELECT * FROM btgwwlogin WHERE passwords = '$getpass'";
-	$result = $conn->query($sql);
-  		// output data of each row
-  		while($row = $result->fetch_assoc()) 
-  		{
-			$adminlevel = $row["userlevel"];
-			$usernames = $row["usernames"];
-			$passwords = $row["passwords"];
-  		}
-	
-	if ($result->num_rows > 0) {
-?>
-
+<?php include 'header.php';?>
+<!------- add page here -------->
+<center>    
+<font color="darkblue" size="4">Add a Contact</font><br><br /><br />
+<form action="add2.php" method="get" name="add1">
+<table style="width:300px;" border="1">
+	<tr>
+    	<td width="111" align="right">
+        	First Name:
+        </td>
+        <td align="left" colspan="3">
+        	<INPUT name="first"
+		 			 style="WIDTH: 100%; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56
+					 require>
+        </td>
+    </tr>
+    <tr>
+       	<td align="right" nowrap>
+        	Last Name:
+        </td>
+        <td align="left" colspan="3">
+        	<INPUT name="last"
+		 			 style="WIDTH: 100%; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56>
+        </td>
+    </tr>
+    <tr>
+    	<td align="right" nowrap>
+        	Gender:
+        </td>
+        <td width="100">
+        	<select size="1" 
+            		name="gender" 
+                    style="WIDTH: 125px; HEIGHT: 22px; background-color: white; color: darkblue;">
+		 				<option>Select Gender</option>
+						<option>Male</option>
+						<option>Female</option>
+        	</select>
+        </td>
+        <td width="104" align="right">
+        	Age:
+        </td>
+        <td width="50">
+            <INPUT name="age"
+		 			 style="WIDTH: 50px; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56>
+        </td>
+    </tr>
+    <tr>
+    	<td align="right" nowrap>
+        	Email Address:
+        </td>
+        <td align="left" colspan="3">
+        	<INPUT name="email"
+		 			 style="WIDTH: 100%; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56>
+        </td>
+    </tr>
+    <tr>
+    	<td align="right" nowrap>
+        	Street Address:
+        </td>
+        <td colspan="3">
+        	<INPUT name="address"
+		 			 style="WIDTH: 100%; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56>
+        </td>
+    </tr>
+    <tr>
+    	<td align="right" nowrap>
+        	City:
+        </td>
+        <td align="left" colspan="3">
+        	<INPUT name="city"
+		 			 style="WIDTH: 100%; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56>
+        </td>
+    </tr>
+    <tr>
+    	<td align="right" nowrap>
+        	State:
+        </td> 
+    	<td align="left">
+			<INPUT name="state"
+		 			 style="WIDTH: 100px; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56>
+        </td>
+        <td align="right" nowrap>
+            Postal Code:
+        </td>
+        <td>
+        	<INPUT name="zip"
+		 			 style="WIDTH: 50px; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56>
+		</td>
+    </tr>
+    <tr>
+    	<td align="right" nowrap>
+        	County:
+        </td>
+        <td align="left" colspan="3">
+        	<INPUT name="county"
+		 			 style="WIDTH: 100%; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56>
+        </td>
+    </tr>
+    <tr>
+    	<td align="right" nowrap>
+        	Phone:
+        </td>
+        <td align="left" colspan="3">
+        	<INPUT name="phone"
+		 			 style="WIDTH: 100%; HEIGHT: 22px; background-color: white; color: darkblue;px"
+		 			 size=56>
+        </td>
+    </tr>
+    <tr>
+    	<td align="right" nowrap="nowrap">
+        	Willing to Bridge:
+        </td>
+        <td align="left">
+        	<select size="1" 
+            		name="interest" 
+                    style="WIDTH: 150px; HEIGHT: 26px; background-color: white; color: darkblue;">
+		 				<option>Select Facility Type</option>	
+						<option>Both</option>
+						<option>Corrections</option>
+                        <option>Treatment</option>
+        	</select>
+        </td>
+    	<td align="right" colspan="1" nowrap>
+        	District:
+        </td>
+        <td align="left">
+        	<INPUT name="district"
+		 			 style="WIDTH: 50px; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=4>
+        </td>
+    </tr>
+	<tr>
+    	<td align="right" nowrap>
+        	Spanish Speaking
+        </td>
+        <td nowrap colspan="3">
+        	<input type="checkbox" name="flang">
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Other Language:&nbsp;
+        	<INPUT name="olang"
+		 			 style="WIDTH: 50px; HEIGHT: 22px; background-color: white; color: darkblue;"
+		 			 size=56>
+		</td>
+    </tr>
+    <tr>
+		<td>
+			&nbsp;<br>
+		</td>	
+		<td align="center" colspan="3">
+			<input type="hidden" name="passwords" value="<?php echo $passwords;?>">
+			<input type="Hidden" name="usernames" value="<?php echo $usernames;?>">
+			<input type="hidden" name="adminlevel" value="<?php echo $adminlevel;?>">
+			<input type="Submit" value="Add Contact" name="b2">
+		</td>
+	</tr>
+</table>
+</form>                    
+</center>                    
+ <!------- end page here ------------>                   
+</body>
+</html> 
 <center>
 
 	<?php
