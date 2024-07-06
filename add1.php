0c139d66d90bdc53a5d056ed8cf1d12a1c305c7c
Cheri Jones <88801930+CheriJones@users.noreply.github.com>
2024-07-06 17:42:01 -0500

    ready to go

--- add1.php
+++ add1.php
@@ -1,120 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-<body>
-<!--- get username and password from database and compare to passed along --->
-<?php
-	$servername = "mysql24.ezhostingserver.com";
-	$username = "cheri";
-	$password = "M@keC0ntact";
-	$dbname = "btgwwtest";
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-  		die("Connection failed: " . $conn->connect_error);
-	}
-
-	$getpass = $_GET["passwords"];
-		
-	$sql = "SELECT * FROM btgwwlogin WHERE passwords = '$getpass'";
-	$result = $conn->query($sql);
-  		// output data of each row
-  		while($row = $result->fetch_assoc()) 
-  		{
-			$adminlevel = $row["userlevel"];
-			$usernames = $row["usernames"];
-			$passwords = $row["passwords"];
-  		}
-	
-	if ($result->num_rows > 0) {
-?>
-
-<center>
-
-	<?php
-		if ($adminlevel > 1) { 
-
-	?>
-		<center>
-			<table cellpadding="0" cellspacing="0" border="0" bordercolor="red" align="center">
-			    <tr>
-			    	<td valign="top">
-						<form method="get" action="admin3.php"name="button21">
-							<input type="hidden" name="passwords" value="<?php echo $passwords;?>">
-							<input type="Hidden" name="usernames" value="<?php echo $usernames;?>">
-							<input type="hidden" name="adminlevel" value="<?php echo $adminlevel;?>">
-            				<input type="hidden" name="district" value="<?php echo $district;?>">
-				        	<input type="submit" value="Add an Admin" name="b21" style="width:260px; height: 24px; background-color: lightgray;" size=35>
-						</form>
-			        </td>
-			    	<td valign="top">
-						<form method="get" action="admin4.php"name="button22">
-							<input type="hidden" name="passwords" value="<?php echo $passwords;?>">
-							<input type="Hidden" name="usernames" value="<?php echo $usernames;?>">
-							<input type="hidden" name="adminlevel" value="<?php echo $adminlevel;?>">
-            				<input type="hidden" name="district" value="<?php echo $district;?>">
-				        	<input type="submit" value="Update an Admin" name="b22" style="width:260px; height: 24px; background-color: lightgray;" size=35>
-						</form>
-			        </td>
-			    	<td valign="top">
-						<form method="get" action="admin5.php"name="button23">
-							<input type="hidden" name="passwords" value="<?php echo $passwords;?>">
-							<input type="Hidden" name="usernames" value="<?php echo $usernames;?>">
-							<input type="hidden" name="adminlevel" value="<?php echo $adminlevel;?>">
-            				<input type="hidden" name="district" value="<?php echo $district;?>">
-				        	<input type="submit" value="Delete an Admin" name="b23" style="width:260px; height: 24px; background-color: lightgray;" size=35>
-						</form>
-			        </td>
-				</tr>
-			</table>
-        <center>
-	
-	
-	<?php }  ?>
-    
-		<center>
-			<table cellpadding="0" cellspacing="0" border="0" bordercolor="red" align="center">
-			    <tr>
-			    	<td valign="baseline">
-						<form method="get" action="add1.php" name="button11">
-							<input type="hidden" name="passwords" value="<?php echo $passwords;?>">
-							<input type="Hidden" name="usernames" value="<?php echo $usernames;?>">
-							<input type="hidden" name="adminlevel" value="<?php echo $adminlevel;?>">
-            				<input type="hidden" name="district" value="<?php echo $district;?>">
-		    	        	<input type="submit" value="Add a Contact" name="b11" style="width:260px; height: 24px; background-color: whitesmoke;" size=35 />
-			          	</form>
-					</td>
-			    	<td valign="baseline">
-						<form method="get" action="update1.php"name="button12">
-							<input type="hidden" name="passwords" value="<?php echo $passwords;?>">
-							<input type="Hidden" name="usernames" value="<?php echo $usernames;?>">
-							<input type="hidden" name="adminlevel" value="<?php echo $adminlevel;?>">
-            				<input type="hidden" name="district" value="<?php echo $district;?>">
-				        	<input type="submit" value="Update Contact Info" name="b12" style="width:260px; height: 24px; background-color: whitesmoke;" size=35>
-						</form>
-			        </td>
-			    	<td valign="baseline">
-						<form method="get" action="delete1.php" name="button13">
-							<input type="hidden" name="passwords" value="<?php echo $passwords;?>">
-							<input type="Hidden" name="usernames" value="<?php echo $usernames;?>">
-							<input type="hidden" name="adminlevel" value="<?php echo $adminlevel;?>">
-            				<input type="hidden" name="district" value="<?php echo $district;?>">
-		    	        	<input type="submit" value="Delete a Contact" name="b13" style="width:260px; height: 24px; background-color: whitesmoke;" size=35 />
-			          	</form>
-					</td>
-				</tr>
-			</table>
-        <center>
-		   
-
+<?php include 'header.php';?>
 <!------- add page here -------->
-
-  
 <center>    
 <font color="darkblue" size="4">Add a Contact</font><br><br /><br />
 <form action="add2.php" method="get" name="add1">
@@ -125,7 +10,6 @@
         </td>
         <td align="left" colspan="3">
         	<INPUT name="first"
-		 			 message="Please enter the first name"
 		 			 style="WIDTH: 268px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
         </td>
@@ -136,8 +20,6 @@
         </td>
         <td align="left" colspan="3">
         	<INPUT name="last"
-		 			 message="Please enter the last name"
-		 			 required="yes"
 		 			 style="WIDTH: 268px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
         </td>
@@ -160,7 +42,6 @@
         </td>
         <td width="50">
             <INPUT name="age"
-		 			 message="Please enter contact age"
 		 			 style="WIDTH: 50px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
         </td>
@@ -171,7 +52,6 @@
         </td>
         <td align="left" colspan="3">
         	<INPUT name="email"
-		 			 message="Please enter the email address"
 		 			 style="WIDTH: 268px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
         </td>
@@ -182,7 +62,6 @@
         </td>
         <td colspan="3">
         	<INPUT name="address"
-		 			 message="Please enter the complete street address"
 		 			 style="WIDTH: 268px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
         </td>
@@ -193,7 +72,6 @@
         </td>
         <td align="left" colspan="3">
         	<INPUT name="city"
-		 			 message="Please enter the city name"
 		 			 style="WIDTH: 268px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
         </td>
@@ -204,7 +82,6 @@
         </td> 
     	<td align="left">
 			<INPUT name="state"
-		 			 message="Please enter the state name"
 		 			 style="WIDTH: 100px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
         </td>
@@ -213,7 +90,6 @@
         </td>
         <td>
         	<INPUT name="zip"
-		 			 message="Please enter your the code"
 		 			 style="WIDTH: 50px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
 		</td>
@@ -224,8 +100,6 @@
         </td>
         <td align="left" colspan="3">
         	<INPUT name="county"
-		 			 message="Please enter the name of the county they live in - this will be used to more closely match release location"
-		 			 required="yes"
 		 			 style="WIDTH: 268px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
         </td>
@@ -236,8 +110,6 @@
         </td>
         <td align="left" colspan="3">
         	<INPUT name="phone"
-		 			 message="Please enter the phone number"
-		 			 required="yes"
 		 			 style="WIDTH: 268px; HEIGHT: 22px; background-color: white; color: darkblue;px"
 		 			 size=56>
         </td>
@@ -260,8 +132,6 @@
         </td>
         <td align="left">
         	<INPUT name="district"
-		 			 message="Please enter the number of the District"
-		 			 required="yes"
 		 			 style="WIDTH: 50px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=4>
         </td>
@@ -280,7 +150,6 @@
         </td>
         <td>
         	<INPUT name="olang"
-		 			 message="Please enter your the code"
 		 			 style="WIDTH: 50px; HEIGHT: 22px; background-color: white; color: darkblue;"
 		 			 size=56>
 		</td>
@@ -297,16 +166,5 @@
 </form>                    
 </center>                    
  <!------- end page here ------------>                   
-                    
-<?php 
-	}else	{
-
-  echo "Have a bad day!";
- }
-
-$conn->close();
-	
-?> 
-
 </body>
 </html> 
